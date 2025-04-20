cce258bce17749dadcff19b533129c1ad9292bcb
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-04-20 21:37:15 +0530

    Install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1 className="text-5xl font-extrabold bg-gradient-to-r from-pink-500 to-emerald-500 via-purple-500 bg-clip-text text-transparent w-fit mx-auto mt-[30vh]">
+        Hello World!
+      </h1>
+    </>
   );
 }
 
