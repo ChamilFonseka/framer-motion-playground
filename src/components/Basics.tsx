1164d29a6709e636483f4b97ab8f010a3f8f120f
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-04-28 20:59:36 +0530

    Basics

--- src/components/Basics.tsx
+++ src/components/Basics.tsx
@@ -1,6 +1,51 @@
+import { motion } from "motion/react";
+
 function Basics() {
   return (
-    <div>Basics</div>
-  )
+    <section className="overflow-hidden p-8">
+      <h1 className="text-5xl font-bold my-8 text-amber-500 underline">Basics</h1>
+      <div className="flex flex-col gap-8">
+
+      <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
+          animate={{ rotate: 360 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+        >
+          Rotate
+        </motion.div>
+
+        <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
+          animate={{ rotateY: 180 }}
+          transition={{ duration: 2, ease: "easeInOut" }}
+          style={{ originX: 1}}
+        >
+          Rotate
+        </motion.div>
+
+        <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 2, ease: "linear" }}
+        >
+          Scale
+        </motion.div>
+
+        <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
+            initial={{ x: '100vw' }}
+            animate={{ x: 0 }}
+            transition={{ type: 'spring', stiffness: 60 }}
+        >
+          X
+        </motion.div>
+
+        <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
+            initial={{ x: 0 }}
+            animate={{ x: '50vw' }}
+            transition={{ duration: 2, ease: "easeInOut" }}
+        >
+          X
+        </motion.div>
+      </div>
+    </section>
+  );
 }
-export default Basics
+export default Basics;
