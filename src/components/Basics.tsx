77a5e32279ac38685dc20e6fc2334db96f77ada8
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-05-09 20:11:37 +0530

    Formatting

--- src/components/Basics.tsx
+++ src/components/Basics.tsx
@@ -2,13 +2,13 @@ import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
 
 function Basics() {
-  const [isVisible, setIsVisible] = useState(true)
+  const [isVisible, setIsVisible] = useState(true);
   return (
     <section className="overflow-hidden p-8">
       <h1 className="text-5xl font-bold my-8 text-amber-500 underline">Basics</h1>
       <div className="flex flex-col gap-8">
 
-      <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
+        <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
           animate={{ rotate: 360 }}
           transition={{ duration: 2, ease: "easeInOut" }}
         >
@@ -18,7 +18,7 @@ function Basics() {
         <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
           animate={{ rotateY: 180 }}
           transition={{ duration: 2, ease: "easeInOut" }}
-          style={{ originX: 1}}
+          style={{ originX: 1 }}
         >
           Rotate
         </motion.div>
@@ -32,19 +32,34 @@ function Basics() {
         </motion.div>
 
         <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
-            initial={{ x: '100vw' }}
-            animate={{ x: 0 }}
-            transition={{ type: 'spring', stiffness: 60 }}
+          initial={{ x: '100vw' }}
+          animate={{ x: 0 }}
+          transition={{ type: 'spring', stiffness: 60 }}
         >
           X
         </motion.div>
 
         <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
-            initial={{ x: 0 }}
-            animate={{ x: '50vw' }}
-            transition={{ duration: 2, ease: "easeInOut" }}
+          initial={{ x: 0 }}
+          animate={{ x: '50vw' }}
+          transition={{ duration: 2, ease: "easeInOut" }}
         >
           X
+        </motion.div>
+
+        <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
+          animate={{
+            x: [0, 100, 100, 0, 0],
+            y: [0, 0, 100, 100, 0]
+          }}
+          transition={{
+            duration: 4,
+            times: [0, 0.25, 0.5, 0.75, 1],
+            ease: "linear",
+            repeat: Infinity
+          }}
+        >
+          M
         </motion.div>
 
         <motion.div className="size-[100px] bg-cyan-500 text-white grid place-items-center"
@@ -56,25 +71,25 @@ function Basics() {
         </motion.div>
 
         <div className="h-[200px] flex items-center gap-4">
-            <AnimatePresence initial={false}>
-                {isVisible ? (
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        exit={{ opacity: 0, scale: 0 }}
-                        className="size-[100px] bg-cyan-500 text-white grid place-items-center"
-                        key="box"
-                    />
-                ) : null}
-            </AnimatePresence>
-            <motion.button
-                className="bg-cyan-600 text-white p-2 rounded-md h-10"
-                onClick={() => setIsVisible(!isVisible)}
-                whileTap={{ y: 1 }}
-                layout
-            >
-                {isVisible ? "Hide" : "Show"}
-            </motion.button>
+          <AnimatePresence initial={false}>
+            {isVisible ? (
+              <motion.div
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+                className="size-[100px] bg-cyan-500 text-white grid place-items-center"
+                key="box"
+              />
+            ) : null}
+          </AnimatePresence>
+          <motion.button
+            className="bg-cyan-600 text-white p-2 rounded-md h-10"
+            onClick={() => setIsVisible(!isVisible)}
+            whileTap={{ y: 1 }}
+            layout
+          >
+            {isVisible ? "Hide" : "Show"}
+          </motion.button>
         </div>
       </div>
     </section>
